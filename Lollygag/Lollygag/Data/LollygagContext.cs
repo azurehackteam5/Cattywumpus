f5c936ec863ba1f186644747aa42e044e48bc8f8
dipakmahajan <dipak.mahajan@ey.com>
2018-08-02 10:07:55 -0400

    Added Products Controller and placeholder view

---
+++ Lollygag/Lollygag/Data/LollygagContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lollygag.Models
+{
+    public class LollygagContext : DbContext
+    {
+        public LollygagContext (DbContextOptions<LollygagContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lollygag.Models.Product> Product { get; set; }
+    }
+}
